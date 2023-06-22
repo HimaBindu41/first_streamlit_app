97196cb3269581240348d0e28e3a498cd3d57a63
HimaBindu41 <118792478+HimaBindu41@users.noreply.github.com>
2023-06-22 17:46:51 +0530

    Update streamlit_app.py
    next changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,12 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 # streamlit.dataframe(my_fruit_list)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+ 
 
 streamlit.header("Fruityvice Fruit Advice!")
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
